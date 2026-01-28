5097b414554b37d0972adb3c92a06324cebfea24
TSS-35 <t.sturza35@gmail.com>
2026-01-27 18:57:33 -0800

    feat: LLM-ready schemas from SeaORM models and gRPC request execution with tonic

--- build.rs
+++ build.rs
@@ -2,7 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::configure()
         .compile(
-            &["src/v1/agent/service.proto"],
+            &[
+                "src/v1/agent/service.proto",
+                "src/v1/query/service.proto",
+                "src/v1/schema/service.proto",
+            ],
             &["src"],
         )?;
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,12 @@ pub mod v1 {
     pub mod agent {
         tonic::include_proto!("v1.agent");
     }
+
+    pub mod query {
+        tonic::include_proto!("v1.query");
+    }
+
+    pub mod schema {
+        tonic::include_proto!("v1.schema");
+    }
 }
